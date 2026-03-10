61409e120b54433dc08a211aba11cc1539ddb42b
John Mills <jmills@lawbulletinmedia.com>
2026-03-10 15:08:19 -0500

    handling error for duplicate url

--- src/lib/db/queries/posts.ts
+++ src/lib/db/queries/posts.ts
@@ -3,6 +3,11 @@ import { db } from "..";
 import { feeds, posts } from "../schema";
 
 export async function createPost(title: string, url: string, description: string | null, publishedAt: Date | null, feedId: string) {
+    // url is unique, so if a post with the same url already exists, we can skip it
+    const existingPost = await db.select().from(posts).where(eq(posts.url, url)).limit(1);
+    if (existingPost.length > 0) {
+        return null;
+    }
     const [result] = await db.insert(posts).values({ title, url, description, publishedAt, feedId }).returning();
     return result;
 }
